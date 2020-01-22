e6aa7c7ec7216b931ba444cf10f8a65b543deac9
Alexander Norinchak <norinchak@gmail.com>
2020-01-22 13:43:47 +0200

    Finalize

--- iitc-plugin-draw-tools-sync.user.js
+++ iitc-plugin-draw-tools-sync.user.js
@@ -95,7 +95,7 @@ function wrapper(plugin_info) {
             gapi.client.drive.files.list({
                 spaces: 'appDataFolder',
                 fields: 'files(id, name)',
-                orderBy: 'modifiedTime'
+                orderBy: 'modifiedTime desc'
             }).then(function(resp) {
                 if(callback) callback(resp.result.files);
             });
@@ -140,7 +140,6 @@ function wrapper(plugin_info) {
     GoogleDriveStorage.prototype.readFile = function(id, callback) {
         gapi.client.load('drive', 'v3').then(function() {
             gapi.client.drive.files.get({fileId: id, alt: 'media'}).then(function(resp) {
-                console.log('File loaded', resp);
                 if(callback) callback(resp);
             });
         });
@@ -154,7 +153,6 @@ function wrapper(plugin_info) {
                 params: {uploadType: 'media'},
                 body: content
             }).then(function(resp) {
-                console.log('File saved', resp);
                 if(callback) callback(resp);
             });
         });
@@ -164,8 +162,6 @@ function wrapper(plugin_info) {
         var self = this;
 
         self.findFile(name, function(file) {
-            console.log("found", file);
-
             if(file) {
                 self.saveFileById(file.id, content, callback);
             }
@@ -180,7 +176,6 @@ function wrapper(plugin_info) {
     GoogleDriveStorage.prototype.deleteFile = function(id, callback) {
         gapi.client.load('drive', 'v3').then(function() {
             gapi.client.drive.files.delete({fileId: id}).then(function() {
-                console.log('File deleted');
                 if(callback) callback();
             });
         });
@@ -206,9 +201,9 @@ function wrapper(plugin_info) {
     }
 
     function setupCSS() {
-        //var css = '#spyAgentBox{display:none;position:absolute!important;z-index:5001;top:50px;left:70px;width:500px;height:450px;overflow:hidden;background:rgba(8,48,78,.9);border:1px solid #20a8b1;color:#ffce00;padding:8px}#spyAgentBox #topBar{height:15px!important}#spyAgentBox #topBar *{height:14px!important}#spyAgentBox .handle{width:89%;text-align:center;color:#fff;line-height:6px;cursor:move;float:right}#spyAgentBox #topBar .btn{display:block;width:10%;cursor:pointer;color:#20a8b1;font-weight:700;text-align:center;line-height:13px;font-size:18px;border:1px solid #20a8b1;float:left}#spyAgentBox #topBar .btn:hover{color:#ffce00;text-decoration:none}#spyAgentsList{background:#000;border:1px solid #20a8b1;color:#fff;height:20px;width:170px;margin-right:38px}#spyAgentHistory{background:#000;border:1px solid #20a8b1;color:#fff;height:20px;width:100px}#spyAgentControls{border-top:1px solid #20a8b1;border-bottom:1px solid #20a8b1;padding-top:4px;padding-bottom:4px;clear:both}#spyAgentStart{background:rgba(8,48,78,1);border:1px solid #20a8b1;color:#ffce00;height:20px}#spyAgentContent{width:100%;height:385px;overflow:auto;overflow-x:hidden;font-size:12px;border-bottom:1px solid #20a8b1}#spyAgentStatus{padding-top:8px;padding-bottom:4px;font-size:12px;color:#fff;float:left}#spyAgentsListContainer{padding-top:4px;padding-bottom:4px;font-size:12px;color:#fff;float:right}.spyAgentContentRow{color:#fff;white-space:nowrap}.spyAgentContentDate{color:#999}.spyAgentRESISTANCE{color:#08f}.spyAgentENLIGHTENED{color:#03dc03}#spyAgentDateSelector{text-align:center;color:#fff;padding:4px;border-bottom:1px solid #20a8b1;display:none}#spyAgentDateSelector button{background:rgba(8,48,78,1);border:1px solid #20a8b1;color:#ffce00;height:20px}';
-        //$('<style>').prop('type', 'text/css').html(css).appendTo('head');
-        $('<link rel="stylesheet" type="text/css" href="http://localhost/dt.css"/>').appendTo('head');
+        var css = '#drawToolsSyncBox{display:none;position:absolute!important;z-index:5001;top:50px;left:60px;width:200px;height:250px;overflow:hidden;background:rgba(8,48,78,.9);border:1px solid #20a8b1;color:#ffce00;padding:8px;font-size:13px;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}#drawToolsSyncBox #drawToolsSyncTopBar{height:15px!important}#drawToolsSyncBox #drawToolsSyncTopBar *{height:14px!important}#drawToolsSyncBox .handle{width:89%;text-align:center;color:#fff;line-height:6px;cursor:move;float:right}#drawToolsSyncBox #drawToolsSyncTopBar .btn{display:block;width:10%;cursor:pointer;color:#20a8b1;font-weight:700;text-align:center;line-height:13px;font-size:18px;border:1px solid #20a8b1;float:left}#drawToolsSyncBox #drawToolsSyncTopBar .btn:hover{color:#ffce00;text-decoration:none}#drawToolsSyncBox #drawToolsSyncTitle{font-size:12px;padding-top:5px}#drawToolsSyncBox #drawToolsSyncList{clear:both;margin-top:8px;height:200px;overflow-x:hidden;overflow-y:auto;border-bottom:1px solid #20a8b1}#drawToolsSyncBox #drawToolsSyncLock{display:none;position:absolute!important;left:0;top:26px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,.5)}#drawToolsSyncBox #drawToolsSyncLock svg{display:block;margin:auto;margin-top:80px}#drawToolsSyncBox #drawToolsSyncAuth{padding-top:24px}#drawToolsSyncBox #drawToolsSyncAuth a{display:block;color:#ffce00;border:1px solid #ffce00;padding:3px 0;margin:10px auto;width:80%;text-align:center;background:rgba(8,48,78,.9)}.drawToolsSyncItem{margin-top:2px;margin-bottom:2px;padding:4px;background:rgba(8,48,78,.75);cursor:pointer;color:#fff}.drawToolsSyncItem:hover{background:rgba(4,24,39,1)}#drawToolsSyncLoadPanel{padding-top:8px}#drawToolsSyncLoadPanel label{display:block;float:left;padding-top:2px;padding-left:5px}#drawToolsSyncLoadPanel input{display:block;float:left}#drawToolsSyncSaveName{width:100px;height:18px}#drawToolsSyncSavePanel{padding-top:6px;text-align:center}#drawToolsSyncSavePanel input{color:#fff}#drawToolsSyncSavePanel button{color:#ffce00;border:1px solid #ffce00;text-align:center;background:rgba(8,48,78,.9)}.drawToolsSyncDeleteButton{float:left;width:14px;height:14px;border:1px solid #f33;background:#000;color:#f33;text-align:center;font-weight:700;margin-right:8px}.drawToolsSyncDeleteButton:hover{background:#900}';
+        $('<style>').prop('type', 'text/css').html(css).appendTo('head');
+        //'<link rel="stylesheet" type="text/css" href="http://localhost/dt.css"/>').appendTo('head');
     }
 
     function setupUI() {
@@ -222,7 +217,7 @@ function wrapper(plugin_info) {
         content += '    <div id="drawToolsSyncContent">';
         content += '        <div id="drawToolsSyncList"></div>';
         content += '        <div id="drawToolsSyncLoadPanel"><input type="checkbox" id="drawToolsSyncAutoClose" onchange="window.plugin.drawToolsSync.onAutoCloseChange()"/> <label for="drawToolsSyncAutoClose">Close after saving or loading</label> </div>';
-        content += '        <div id="drawToolsSyncSavePanel"></div>';
+        content += '        <div id="drawToolsSyncSavePanel">Name: <input type="text" id="drawToolsSyncSaveName"/> <button onclick="window.plugin.drawToolsSync.saveFile($(\'#drawToolsSyncSaveName\').val())">Save</button></div>';
         content += '    </div>';
         content += '    <div id="drawToolsSyncAuth"><a onclick="window.plugin.drawToolsSync.auth();return false;">Authorize</a></div>';
         content += '    <div id="drawToolsSyncLock"><svg width="50px" height="50px" viewBox="-103 -16 533 533.33333" xmlns="http://www.w3.org/2000/svg"><path fill="white" d="m293.167969 131.632812v-35.382812h5.222656c12.847656.035156 23.332031-10.269531 23.527344-23.113281v-49.988281c-.191407-12.851563-10.671875-23.164063-23.527344-23.148438h-280.449219c-12.855468-.015625-23.335937 10.296875-23.523437 23.148438v49.988281c.195312 12.84375 10.675781 23.148437 23.523437 23.113281h5.226563v35.382812c-.019531 16.757813 7.617187 32.605469 20.734375 43.03125l94.039062 75.183594-92.191406 71.511719c-13.496094 10.398437-21.386719 26.496094-21.332031 43.539063v38.851562h-6.476563c-12.847656-.035156-23.328125 10.269531-23.523437 23.113281v49.988281c.1875 12.851563 10.667969 23.164063 23.523437 23.148438h280.449219c12.855469.015625 23.335937-10.296875 23.527344-23.148438v-49.988281c-.195313-12.84375-10.679688-23.148437-23.527344-23.113281h-3.972656v-38.605469c-.078125-17.210937-8.140625-33.410156-21.824219-43.851562l-94.066406-71.542969 93.921875-75.089844c13.113281-10.425781 20.746093-26.277344 20.71875-43.027344zm-273.75-106.632812h277.5v46.25h-277.5zm277.5 450h-277.5v-46.25h15.34375c.71875 0 1.445312.203125 2.199219.203125.75 0 1.484374-.203125 2.203124-.203125h240.390626c.714843 0 1.449218.203125 2.199218.203125.753906 0 1.484375-.203125 2.199219-.203125h12.964844zm-27.5-109.855469v38.605469h-220v-38.851562c-.019531-9.3125 4.289062-18.105469 11.671875-23.785157l97.140625-75.351562 99.222656 75.425781c7.484375 5.703125 11.90625 14.550781 11.964844 23.957031zm-12.605469-210.007812-98.621094 78.859375-98.65625-78.859375c-7.179687-5.6875-11.367187-14.34375-11.367187-23.503907v-35.382812h220v35.382812c0 9.160157-4.179688 17.8125-11.355469 23.503907zm0 0"/></svg></div>';
@@ -231,15 +226,11 @@ function wrapper(plugin_info) {
         $('body').append(content);
         $('#drawToolsSyncBox').draggable({handle: '.handle', containment: 'window'});
 
-        if(!window.plugin.drawToolsSync.isSmart) {
-            //Desktop mode
-            $('#toolbox').append('<a onclick="window.plugin.drawToolsSync.showBox(0);return false;">DrawTools Load</a>');
-            $('#toolbox').append('<a onclick="window.plugin.drawToolsSync.showBox(1);return false;">DrawTools Save</a>');
-        }
-        else {
+        if(window.useAndroidPanes()) {
             //Mobile mode
-            if(window.useAndroidPanes()) android.addPane("plugin-draw-tools-sync-load", "DrawTools Load", "ic_action_star");
-            if(window.useAndroidPanes()) android.addPane("plugin-draw-tools-sync-save", "DrawTools Save", "ic_action_star");
+            android.addPane("plugin-draw-tools-sync-load", "DrawTools Load", "ic_action_star");
+            android.addPane("plugin-draw-tools-sync-save", "DrawTools Save", "ic_action_save");
+
             window.addHook('paneChanged', function(pane) {
                 if(pane === 'plugin-draw-tools-sync-load') {
                     showBox(0);
@@ -248,7 +239,16 @@ function wrapper(plugin_info) {
                 if(pane === 'plugin-draw-tools-sync-save') {
                     showBox(1);
                 }
+
+                setTimeout(function() {
+                    window.show('map');
+                }, 0);
             });
+        }
+        else {
+            //Desktop mode
+            $('#toolbox').append('<a onclick="window.plugin.drawToolsSync.showBox(0);return false;">DrawTools Load</a>');
+            $('#toolbox').append('<a onclick="window.plugin.drawToolsSync.showBox(1);return false;">DrawTools Save</a>');
         }
     }
 
@@ -260,20 +260,19 @@ function wrapper(plugin_info) {
 
         showLock();
 
+        $("#drawToolsSyncList").html("");
+
         window.plugin.drawToolsSync.getFilesList(function(list) {
             if(currentRefresh !== refreshFilesListId) return;
-
-            $("#drawToolsSyncList").html("");
-
-            var t = list.slice();
-            for(i=0; i<20; i++) {
-                list = list.concat(t);
-            }
 
             for(var i=0; i<list.length; i++) {
                 var file = list[i];
                 if(mode === 0) {
                     $("#drawToolsSyncList").append("<div class='drawToolsSyncItem' onclick='window.plugin.drawToolsSync.loadFile(\"" + file.name + "\")'><b>" + file.name + "</b></div>");
+                }
+
+                if(mode === 1) {
+                    $("#drawToolsSyncList").append("<div class='drawToolsSyncItem' onclick='window.plugin.drawToolsSync.saveFile(\"" + file.name + "\")'><div class='drawToolsSyncDeleteButton' onclick='window.plugin.drawToolsSync.deleteFile(\"" + file.name + "\"); event.cancelBubble=true;'>X</div><b>" + file.name + "</b></div>");
                 }
             }
 
@@ -347,6 +346,7 @@ function wrapper(plugin_info) {
         setupUI();
 
         $("#drawToolsSyncAutoClose").prop('checked', !!options.autoClose);
+        $("#drawToolsSyncSaveName").val(options.lastSave || 'default');
 
         if(options.boxPositionX !== undefined) $('#drawToolsSyncBox').css('left', options.boxPositionX + 'px');
         if(options.boxPositionY !== undefined) $('#drawToolsSyncBox').css('top', options.boxPositionY + 'px');
@@ -356,7 +356,6 @@ function wrapper(plugin_info) {
 
         dataStorage.init(function() {
             dataStorage.authorize(false, function(authorized) {
-                console.log("Authorized status:", authorized);
                 ready = true;
             });
         });
@@ -393,7 +392,28 @@ function wrapper(plugin_info) {
                 return;
             }
 
-            dataStorage.saveFileByName(fixDataFileName(name), data, callback);
+            showLock();
+
+            dataStorage.findFile(fixDataFileName(name), function(file) {
+                if(file) {
+                    if(!confirm("Owerride " + name + " save?")) {
+                        hideLock();
+                        return;
+                    }
+                }
+
+                $('#drawToolsSyncSaveName').val(name);
+
+                dataStorage.saveFileByName(fixDataFileName(name), data, function() {
+                    if(options.autoClose) hideBox();
+                    else refreshFilesList(1);
+
+                    options.lastSave = name;
+                    saveOptions();
+
+                    if(callback) callback();
+                });
+            });
         };
 
         window.plugin.drawToolsSync.loadFile = function(name, callback) {
@@ -429,13 +449,20 @@ function wrapper(plugin_info) {
         };
 
         window.plugin.drawToolsSync.deleteFile = function(name, callback) {
+            if(!confirm("Realy delete " + name + " save?")) return;
+
+            showLock();
+
             dataStorage.findFile(fixDataFileName(name), function(file) {
                 if(!file) {
                     alert("File " + name + " not found");
                     return;
                 }
 
-                dataStorage.deleteFile(file.id, callback);
+                dataStorage.deleteFile(file.id, function() {
+                    refreshFilesList(1);
+                    if(callback) callback();
+                });
             });
         };
 
